1a00ecd79fdde55e009a18737a68e7eda8609bad
Roberto Chavez Jr <rchavezj.ai@gmail.com>
2020-05-29 15:15:27 -0700

    Story section

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React from 'react';
 import Header from './layout/Header/Header';
 import Realtors from './components/Realtors/Realtors';
 import Features from './components/Features/Features';
+import Story from './components/Story/Story';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Header />
       <Realtors />
       <Features />
+      <Story />
     </body>
   );
 }
