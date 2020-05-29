9f75529d145bfb411d0fe24db3a1c8053c778f13
Roberto Chavez Jr <rchavezj.ai@gmail.com>
2020-05-29 13:46:05 -0700

    Added realtors inside the header section

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import './App.scss';
 
 import React from 'react';
 import Header from './layout/Header/Header';
+import Realtors from './components/Realtors/Realtors';
 
 function App() {
   return (
-    <Header />
+    <body className="container">
+      <Header />
+      <Realtors />
+    </body>
   );
 }
 
---
+++ src/components/Realtors/Realtors.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import Realtor1 from '../../img/realtor-1.jpeg';
+import Realtor2 from '../../img/realtor-2.jpeg';
+import Realtor3 from '../../img/realtor-3.jpeg'
+
+const Realtors = () => {
+    return (
+        <div className="realtors">
+            <h3 className="heading-3">Top 3 Realtors</h3>
+            <div className="realtors__list">
+                <img src={Realtor1} alt="Realtor 1" className="realtors__img" />
+                <div className="realtors__details">
+                    <h4 className="heading-4 heading-4--light">Erik Feinman</h4>
+                    <p className="realtors__sold">245 houses sold</p>
+                </div>
+
+                <img src={Realtor2} alt="Realtor 2" className="realtors__img" />
+                <div className="realtors__details">
+                    <h4 className="heading-4 heading-4--light">Kim Brown</h4>
+                    <p className="realtors__sold">212 houses sold</p>
+                </div>
+
+                <img src={Realtor3} alt="Realtor 3" className="realtors__img" />
+                <div className="realtors__details">
+                    <h4 className="heading-4 heading-4--light">Toby Ramsey</h4>
+                    <p className="realtors__sold">198 houses sold</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Realtors;
