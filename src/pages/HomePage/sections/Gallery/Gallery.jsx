c74e194eee6e90765b9b8c306841e828a84068b7
Roberto Chavez Jr <rchavezj.ai@gmail.com>
2020-05-31 10:50:13 -0700

    Created another component to render each house

--- src/pages/HomePage/sections/Gallery/Gallery.jsx
+++ src/pages/HomePage/sections/Gallery/Gallery.jsx
@@ -21,20 +21,20 @@ import gal14 from '../../../../img/gal-14.jpeg';
 const Gallery = () => {
     return (
         <section class="gallery">
-            <figure class="gallery__item gallery__item--1"><img src={gal1} class="gallery__img" /></figure>
-            <figure class="gallery__item gallery__item--2"><img src={gal2} class="gallery__img" /></figure>
-            <figure class="gallery__item gallery__item--3"><img src={gal3} class="gallery__img" /></figure>
-            <figure class="gallery__item gallery__item--4"><img src={gal4} class="gallery__img" /></figure>
-            <figure class="gallery__item gallery__item--5"><img src={gal5} class="gallery__img" /></figure>
-            <figure class="gallery__item gallery__item--6"><img src={gal6} class="gallery__img" /></figure>
-            <figure class="gallery__item gallery__item--7"><img src={gal7} class="gallery__img" /></figure>
-            <figure class="gallery__item gallery__item--8"><img src={gal8} class="gallery__img" /></figure>
-            <figure class="gallery__item gallery__item--9"><img src={gal9} class="gallery__img" /></figure>
-            <figure class="gallery__item gallery__item--10"><img src={gal10} class="gallery__img" /></figure>
-            <figure class="gallery__item gallery__item--11"><img src={gal11} class="gallery__img" /></figure>
-            <figure class="gallery__item gallery__item--12"><img src={gal12} class="gallery__img" /></figure>
-            <figure class="gallery__item gallery__item--13"><img src={gal13} class="gallery__img" /></figure>
-            <figure class="gallery__item gallery__item--14"><img src={gal14} class="gallery__img" /></figure>
+            <figure class="gallery__item gallery__item--1"><img src={gal1} alt="/#" class="gallery__img" /></figure>
+            <figure class="gallery__item gallery__item--2"><img src={gal2} alt="/#" class="gallery__img" /></figure>
+            <figure class="gallery__item gallery__item--3"><img src={gal3} alt="/#" class="gallery__img" /></figure>
+            <figure class="gallery__item gallery__item--4"><img src={gal4} alt="/#" class="gallery__img" /></figure>
+            <figure class="gallery__item gallery__item--5"><img src={gal5} alt="/#" class="gallery__img" /></figure>
+            <figure class="gallery__item gallery__item--6"><img src={gal6} alt="/#" class="gallery__img" /></figure>
+            <figure class="gallery__item gallery__item--7"><img src={gal7} alt="/#" class="gallery__img" /></figure>
+            <figure class="gallery__item gallery__item--8"><img src={gal8} alt="/#" class="gallery__img" /></figure>
+            <figure class="gallery__item gallery__item--9"><img src={gal9} alt="/#" class="gallery__img" /></figure>
+            <figure class="gallery__item gallery__item--10"><img src={gal10} alt="/#" class="gallery__img" /></figure>
+            <figure class="gallery__item gallery__item--11"><img src={gal11} alt="/#" class="gallery__img" /></figure>
+            <figure class="gallery__item gallery__item--12"><img src={gal12} alt="/#" class="gallery__img" /></figure>
+            <figure class="gallery__item gallery__item--13"><img src={gal13} alt="/#" class="gallery__img" /></figure>
+            <figure class="gallery__item gallery__item--14"><img src={gal14} alt="/#" class="gallery__img" /></figure>
         </section>
     )
 }
