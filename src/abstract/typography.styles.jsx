64cdb209abbf603972b8a1374a5a2a9cf703d6ff
Roberto Chavez Jr <rchavezj.ai@gmail.com>
2020-09-20 23:13:07 -0700

    Designing topography css style

--- src/abstract/typography.styles.jsx
+++ src/abstract/typography.styles.jsx
@@ -1,6 +1,38 @@
 import styled from '@emotion/styled';
 
+
+// COLOR VARIABLES
+$color - primary: #c69963;
+$color - primary - dark: #B28451;
+
+$color - secondary: #101d2c;
+
+$color - grey - light - 1: #f9f7f6;
+$color - grey - light - 2: #aaa;
+
+$color - grey - dark - 1: #54483A;
+$color - grey - dark - 2: #6D5D4B;
+
+// FONT VARIABLES
+$font - primary: 'Nunito', sans - serif;
+$font - display: 'Josefin Sans', sans - serif;
+
+// RESPONSIVE BREAKPOINTS
+$bp - largest: 75em;   // 1200px
+$bp - large: 62.5em;   // 1000px
+$bp - medium: 50em;    // 800px;
+$bp - small: 37.5em;    // 600px;
+
+
 export const heading = styled.div`
+    font-weight: 400;
+    font-family: 'Josefin Sans', sans-serif;
+`;
+
+export const heading1 = styled.h1`
+    font-size: 4.5rem;
+    font-style: italic;
+    line-height: 1;
 
 
 `;
