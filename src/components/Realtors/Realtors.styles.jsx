b2e1ed7d57583f927a351987966a49bf78852625
Roberto Chavez Jr <rchavezj.ai@gmail.com>
2020-10-04 13:39:41 -0700

    Finished coding first attempt replacing realtors ui components from sass to twin.macro (emotion + tailwindcss)

--- src/components/Realtors/Realtors.styles.jsx
+++ src/components/Realtors/Realtors.styles.jsx
@@ -20,7 +20,37 @@ const realtors = styled.div`
 
 const list = styled.div`
     ${tw`
-        grid
-        
+        mg_lg:grid
+        mg_lg:gap-x-8
+        mg_lg:items-center
+    `}
+
+    ${tw`
+        sm:realtors_sm
+    `}
+    
+    grid-row-gap: 5vh;
+    ${tw` grid-cols-realtors `}
+    
+`;
+
+
+const img = styled.img`
+    ${tw`
+        w-28
+        block
+    `}border-radius: 50%;
+`;
+
+const sold = styled.p`
+    ${tw`
+        -mt-12
+        uppercase
+        text-gray-50
     `}
 `;
+
+
+export const Realtors = {
+    realtors, list, img, sold
+}
